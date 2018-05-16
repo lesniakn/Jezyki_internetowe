3b7b7b0612b2c818b1da1f5ddb785607fba8bb56
Norbert Leśniak <norbert.lesniak@gmail.com>
2018-05-16 21:30:51 +0200

    Utworzenie modelu Kategoria

---
+++ WSIZ/WSIZ/Models/Kategoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WSIZ.Models
+{
+    public class Kategoria
+    {
+        public int KategoriaID { get; set; }
+        public string NazwaKategorii { get; set; }
+        public string OpisKategorii { get; set; }
+        public virtual ICollection<Przedmiot> Przedmiot { get; set; }
+    }
+}
