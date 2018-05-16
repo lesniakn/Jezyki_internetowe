7d81096c4d9e85007e22bbfb009329db36d805d9
Norbert Leśniak <norbert.lesniak@gmail.com>
2018-05-16 21:29:56 +0200

    Utworzenie modelu Przedmiot

---
+++ WSIZ/WSIZ/Models/Przedmiot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WSIZ.Models
+{
+    public class Przedmiot
+    {
+        public int PrzedmiotID { get; set; }
+        public int KategoriaID { get; set; }
+        public string NazwaPrzedmiotu { get; set; }
+        public string ProducentPrzedmiotu { get; set; }
+        public DateTime DataDodania { get; set; }
+        public string OpisPrzedmiotu { get; set; }
+        public decimal CenaPrzedmiotu { get; set; }
+
+        public virtual ICollection<Kategoria> Kategoria { get; set; }
+    }
+}
