036744bbb58bdcd3f5d37a39905dee9eb70b4041
Norbert Leśniak <norbert.lesniak@gmail.com>
2018-05-16 21:32:56 +0200

    Utworzenie kontekstu bazy danych

---
+++ WSIZ/WSIZ/DAL/ShopContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using WSIZ.Models;
+
+namespace WSIZ.DAL
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext() : base("ShopContext")
+        {
+
+        }
+        public DbSet<Przedmiot> Przedmiot { get; set; }
+        public DbSet<Kategoria> Kategoria { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
